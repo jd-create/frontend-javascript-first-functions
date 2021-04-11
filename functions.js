519c5330e42e1fec9a1820e5a0d3a5d856140356
jd-create <jenniferdemeijer@t-online.de>
2021-04-11 16:45:52 +0200

    tasks functions.js done

--- functions.js
+++ functions.js
@@ -13,21 +13,41 @@ console.log(greeting);
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
+function getSchool() {
+  return "Novi Hogeschool";
+}
 
+console.log(getSchool());
 
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
+const name = "Jenni";
 
+function complimentGiver(name) {
+  return "Grondig bezig " + name + "!";
+}
 
+console.log(complimentGiver(name));
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
+let grades = [8, 8.5, 6, 7]
 
+function getGrades(grades) {
+  return grades;
+}
 
-
+console.log(getGrades(grades));
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+let details = ["Jennifer", "Demeijer-van Driel"]
+const detailsSentence = "firstName: " + details[0] + ", lastName: " + details[1]
+function getDetails() {
+  return detailsSentence;
+}
+
+console.log(getDetails(details));
